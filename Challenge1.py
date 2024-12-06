62e81bb54c433e5beb00e66b0fb68cd9b06d934f
Daraland98 <darandreslopez@proton.me>
2024-12-06 22:28:19 +0100

    feat: solution for the second challenge of the Advent programming challenge

--- Challenge1.py
+++ Challenge1.py
@@ -3,21 +3,7 @@
 # Your task is to write a function that receives a list of integers (which may include duplicates) and returns a new list without duplicates, sorted in ascending order.
 
 def prepare_gifts(gifts):
-    unique_gifts = list(set(gifts))
-    sorted_list = []
-    for item in unique_gifts:
-        low, high = 0, len(sorted_list)
-        while low < high:
-            mid = (low + high) // 2
-            if sorted_list[mid] < item:
-                low = mid + 1
-            else:
-                high = mid
-        sorted_list.insert(low, item)
-    
-    return sorted_list
-
-
+   return sorted(set(gifts))
 
 gifts1 = [3, 1, 2, 3, 4, 2, 5]
 prepared_gifts1 = prepare_gifts(gifts1)
